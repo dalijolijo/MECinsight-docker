6f4514cc0b9bb7d682439f29f72226188bf2020e
David <37987466+dalijolijo@users.noreply.github.com>
2018-09-06 21:33:01 +0200

    Update bootstrap to blocks directory

--- start.sh
+++ start.sh
@@ -3,9 +3,9 @@ set -u
 
 # Downloading bootstrap file
 cd /home/megacoin/bitcore-livenet/bin/mynode/data
-if [ ! -d /home/megacoin/bitcore-livenet/bin/mynode/data/data/blocks ] && [ "$(curl -Is https://${WEB}/${BOOTSTRAP} | head -n 1 | tr -d '\r\n')" = "HTTP/1.1 200 OK" ] ; then \
+if [ ! -d /home/megacoin/bitcore-livenet/bin/mynode/data/blocks ] && [ "$(curl -Is https://${WEB}/${BOOTSTRAP} | head -n 1 | tr -d '\r\n')" = "HTTP/1.1 200 OK" ] ; then \
         wget https://${WEB}/${BOOTSTRAP}; \
-        tar -xvzf ${BOOTSTRAP}; \
+        tar -xf ${BOOTSTRAP}; \
         rm ${BOOTSTRAP}; \
 fi
 
